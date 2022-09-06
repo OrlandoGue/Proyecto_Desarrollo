bacab6c9347d9198372b7a0b04d34e6a045790c5
OrlandoGue <orlandook283@gmail.com>
2022-09-05 19:16:22 -0500

    Correccion de errores Lunes

--- ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
@@ -4,9 +4,9 @@ namespace ProyectoCiclo3.App.Dominio{
  
         public int id {get;set;} 
         public string marca {get;set;}            
-        public string modelo {get;set;}            
+        public int modelo {get;set;}            
         public int kilometraje {get;set;}            
-        public int num_asientos {get;set;}            
+        public int numero_asientos {get;set;}            
         public string placa {get;set;}            
            
     }
