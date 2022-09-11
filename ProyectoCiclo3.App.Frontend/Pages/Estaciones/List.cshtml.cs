50e9980e5b3fdcf1e08cda9635cf76a283e2fa3e
OrlandoGue <orlandook283@gmail.com>
2022-09-10 20:34:38 -0500

    Cambios 10 de Sep

--- ProyectoCiclo3.App.Frontend/Pages/Estaciones/List.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Estaciones/List.cshtml.cs
@@ -14,6 +14,8 @@ namespace ProyectoCiclo3.App.Frontend.Pages
        
     private readonly RepositorioEstaciones repositorioEstaciones;
     public IEnumerable<Estaciones> Estaciones {get;set;}
+    [BindProperty]
+    public Estaciones Estacion {get;set;}
  
     public ListEstacionesModel(RepositorioEstaciones repositorioEstaciones)
     {
@@ -23,6 +25,14 @@ namespace ProyectoCiclo3.App.Frontend.Pages
     public void OnGet()
     {
         Estaciones=repositorioEstaciones.GetAll();
+    }
+    public IActionResult OnPost()
+    {
+        if(Estacion.id>0)
+        {
+            repositorioEstaciones.Delete(Estacion.id);
+        }
+        return RedirectToPage("./List");
     }
     }
 }
