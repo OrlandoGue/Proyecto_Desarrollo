129c22c77b231f80252cc1f546b6c1f436985cd7
OrlandoGue <orlandook283@gmail.com>
2022-09-20 20:29:34 -0500

    Cambios 20 de Sep

--- ProyectoCiclo3.App.Frontend/Pages/Buses/Details.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Buses/Details.cshtml.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProyectoCiclo3.App.Persistencia.AppRepositorios;
 using ProyectoCiclo3.App.Dominio;
+using Microsoft.AspNetCore.Authorization;
  
 namespace ProyectoCiclo3.App.Frontend.Pages
 {
+    [Authorize]
     public class DetailsBusModel : PageModel
-    {
+    {    
        private readonly RepositorioBuses repositorioBuses;
               public Buses Bus {get;set;}
  
